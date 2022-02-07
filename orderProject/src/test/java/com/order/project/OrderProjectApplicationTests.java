b46bf9764d1046c9acc470dbd7ed1da4513f8ed7
Naveen0846 <62131417+Naveen0846@users.noreply.github.com>
2022-02-07 11:50:33 +0530

    Add files via upload

---
+++ orderProject/src/test/java/com/order/project/OrderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
